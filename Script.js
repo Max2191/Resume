c7e376a3d4e5806dea839a216439c3500ca0758c
Max <maxtucker2191@gmail.com>
2018-07-30 21:13:34 +0100

    Layout improved, bootstrap scripts added for components that will follow

---
+++ Script.js
@@ -0,0 +1,10 @@
+/* On hover event - load this card for each project, similar to udemy site*/
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
